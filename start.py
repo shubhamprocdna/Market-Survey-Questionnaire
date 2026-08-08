e790e3815856448566e93284f9d25fb85018ab20
Shubham Pandey <shubham.pandey@procdna.com>
2026-08-08 15:01:50 +0530

    start changes

--- start.py
+++ start.py
@@ -13,7 +13,7 @@ def main():
     
     # We set the working directory to Backend so uvicorn finds the app module
     backend_process = subprocess.Popen(
-        [sys.executable, "-m", "uvicorn", "app.main:app", "--host", "127.0.0.1", "--port", "8000"],
+        ["python", "-m", "uvicorn", "app.main:app", "--host", "127.0.0.1", "--port", "8000"],
         cwd=backend_dir
     )
 
